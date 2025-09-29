a317c854cb4fa1c34a449c1018fc6ff6776fdd87
heloziza <helogiacometti@gmail.com>
2025-09-28 21:04:42 -0300

    docs adicionar exemplos de metodos, atributos e campos

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using ExemploExplorando.Models;
+
+Pessoa p1 = new Pessoa();
+p1.Nome = "Helo";
+p1.Sobrenome = "Giacometti";
+
+Pessoa p2 = new Pessoa();
+p2.Nome = "Leonardo";
+p2.Sobrenome = "Buta";
+
+Curso cursoDeIngles = new Curso();
+cursoDeIngles.Nome = "Inglês";
+cursoDeIngles.Alunos = new List<Pessoa>();
+
+cursoDeIngles.AdicionarAluno(p1);
+cursoDeIngles.AdicionarAluno(p2);
+cursoDeIngles.ListarAlunos();
