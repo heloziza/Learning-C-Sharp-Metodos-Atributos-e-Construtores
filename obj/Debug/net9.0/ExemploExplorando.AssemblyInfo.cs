5f17a779140dc4fdaba24ad812c3f4ab99dfa82a
heloziza <helogiacometti@gmail.com>
2025-10-05 20:54:33 -0300

    docs baixar pacotes com nuget e adicionar explicações nas anotações

--- obj/Debug/net9.0/ExemploExplorando.AssemblyInfo.cs
+++ obj/Debug/net9.0/ExemploExplorando.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf1aa54db2991ec0e00d5f8e23718ca8a3981561")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4017a3c1e56461c07d0a9113e8455e0596a3ce4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
