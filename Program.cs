1a7f3dceb61e63078019127a951899977d23478f
heloziza <helogiacometti@gmail.com>
2025-10-05 21:20:17 -0300

    docs adicionar serialização de arquivo Json utilizando o pacote baixado

--- Program.cs
+++ Program.cs
@@ -1,10 +1,29 @@
 ﻿using System.Globalization;
 using ExemploExplorando.Models;
+using Newtonsoft.Json;
+
+//Serialização: --------------------------------------------------------------------------------
+
+DateTime dataAtual = DateTime.Now;
+List<Venda> listaVendas = new List<Venda>();
+
+Venda v1 = new Venda(1, "Material de escritório", 25.00M, dataAtual);
+Venda v2 = new Venda(2, "Material de farmácia", 105.00M, dataAtual);
+
+listaVendas.Add(v1);
+listaVendas.Add(v2);
+
+string serializado = JsonConvert.SerializeObject(listaVendas, Formatting.Indented);
+
+File.WriteAllText("Arquivos/vendas.json", serializado);
+
+Console.WriteLine(serializado);
+
 
 //IF ternário: --------------------------------------------------------------------------------
-int numero = 10;
-bool ehPar = numero % 2 == 0;
-Console.WriteLine($"O número {numero} é " + (ehPar ? "par" : "ímpar"));
+// int numero = 10;
+// bool ehPar = numero % 2 == 0;
+// Console.WriteLine($"O número {numero} é " + (ehPar ? "par" : "ímpar"));
 
 
 //Desconstrutor: --------------------------------------------------------------------------------
--- obj/Debug/net9.0/ExemploExplorando.AssemblyInfo.cs
+++ obj/Debug/net9.0/ExemploExplorando.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4017a3c1e56461c07d0a9113e8455e0596a3ce4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f17a779140dc4fdaba24ad812c3f4ab99dfa82a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
