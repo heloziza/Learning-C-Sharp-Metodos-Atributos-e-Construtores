4d7da9c1001edbe4cf3d94f3d5f4cd0e34f8908c
heloziza <helogiacometti@gmail.com>
2025-10-05 22:25:43 -0300

    adicionar deserialização e anotações importantes

--- Program.cs
+++ Program.cs
@@ -2,22 +2,35 @@
 using ExemploExplorando.Models;
 using Newtonsoft.Json;
 
+// Deserialização: --------------------------------------------------------------------------------
+
+string conteudoArquivo = File.ReadAllText("Arquivos/vendas.json");
+
+List<Venda> listaVenda = JsonConvert.DeserializeObject<List<Venda>>(conteudoArquivo);
+
+foreach (Venda venda in listaVenda)
+{
+    Console.WriteLine($"Id: {venda.Id}, Produto: {venda.Produto}, Preço: {venda.Preco}, Data de venda: {venda.DataVenda.ToString("dd/MM/yyyy HH:mm")}");
+}
+
+
+
 //Serialização: --------------------------------------------------------------------------------
 
-DateTime dataAtual = DateTime.Now;
-List<Venda> listaVendas = new List<Venda>();
+// DateTime dataAtual = DateTime.Now;
+// List<Venda> listaVendas = new List<Venda>();
 
-Venda v1 = new Venda(1, "Material de escritório", 25.00M, dataAtual);
-Venda v2 = new Venda(2, "Material de farmácia", 105.00M, dataAtual);
+// Venda v1 = new Venda(1, "Material de escritório", 25.00M, dataAtual);
+// Venda v2 = new Venda(2, "Material de farmácia", 105.00M, dataAtual);
 
-listaVendas.Add(v1);
-listaVendas.Add(v2);
+// listaVendas.Add(v1);
+// listaVendas.Add(v2);
 
-string serializado = JsonConvert.SerializeObject(listaVendas, Formatting.Indented);
+// string serializado = JsonConvert.SerializeObject(listaVendas, Formatting.Indented);
 
-File.WriteAllText("Arquivos/vendas.json", serializado);
+// File.WriteAllText("Arquivos/vendas.json", serializado);
 
-Console.WriteLine(serializado);
+// Console.WriteLine(serializado);
 
 
 //IF ternário: --------------------------------------------------------------------------------
